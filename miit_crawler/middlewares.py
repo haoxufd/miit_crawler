e0d4daa28e609637a5518a5ccd400836b6e5f1ff
Hao Xu <xuhao21@m.fudan.edu.cn>
2025-02-28 12:15:42 +0800

    Wait for 0.05s before submitting

--- miit_crawler/middlewares.py
+++ miit_crawler/middlewares.py
@@ -14,6 +14,7 @@ import requests
 from PIL import Image
 import numpy as np
 from captcha_recognizer.recognizer import Recognizer
+import random
 
 from miit_crawler.exceptions import CaptchaRecognitionError, ImageDownloadError
 
@@ -248,6 +249,7 @@ class SeleniumMiddleware(object):
             raise e
 
         # 点击继续访问按钮
+        time.sleep(0.05)
         submit_button.click()
         self.logger.info("已点击提交按钮")
 
